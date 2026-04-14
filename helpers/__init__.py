42a1fdaddba34c539a04bada1cb77fdf4b8a9c3f
gabriel-samadoulougou <gabriel.samadoulougou@cgi.com>
2026-04-14 12:15:29 -0300

    Remove empty helpers folder - using package.helpers instead

--- helpers/__init__.py
+++
@@ -1,4 +0,0 @@
-# Local helper modules - DEPRECATED
-# Use msuliot.helpers instead (embedding_manager, vector_db_manager, chat_manager, etc.)
-
-# This module is kept for backward compatibility but no longer used by app.py
